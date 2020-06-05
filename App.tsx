90166a0c6dad4cb54ec7046b6a982f4b6cbfdfb0
Zweihänder <zweidev@zweihander.me>
2020-06-04 21:58:02 -0400

    Fix(App): Updated react-navigation to v5 and added proper typing

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { View, StyleSheet, Platform, StatusBar } from 'react-native';
 import AddEntry from './components/AddEntry';
 import { createStore, Store } from 'redux';
@@ -6,15 +7,26 @@ import { Provider } from 'react-redux';
 import reducer from './reducers';
 import { RootState } from './types';
 import History from './components/History';
-import { TabNavigator, StackNavigator } from 'react-navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { purple, white } from './utils/colors';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
-import { Constants } from 'expo';
-import Live from './components/Live'
-import { setLocalNotification } from './utils/helpers'
+import Constants from 'expo-constants';
+import EntryDetail from './components/EntryDetail';
+import Live from './components/Live';
+import { setLocalNotification } from './utils/helpers';
 
+interface TMGStatusBarProps {
+	backgroundColor: string;
+	barStyle?: StatusBar['props']['barStyle'];
+}
 
-const TMGStatusBar: React.FC = ({ backgroundColor, ...props }) => {
+const TMGStatusBar: React.FC<TMGStatusBarProps> = ({
+	backgroundColor,
+	...props
+}) => {
 	return (
 		<View style={{ backgroundColor, height: Constants.statusBarHeight }}>
 			<StatusBar
@@ -24,64 +36,135 @@ const TMGStatusBar: React.FC = ({ backgroundColor, ...props }) => {
 			/>
 		</View>
 	);
-}
+};
 
-const Tabs = TabNavigator({
-  History: {
-    screen: History,
-    navigationOptions: {
-      tabBarLabel: 'History',
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
-    },
-  },
-  AddEntry: {
-    screen: AddEntry,
-    navigationOptions: {
-      tabBarLabel: 'Add Entry',
-      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
-    },
-  },
-  Live: {
-    screen: Live,
-    navigationOptions: {
-      tabBarLabel: 'Live',
-      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
-    }
-  }
-}, {
-  navigationOptions: {
-    header: null
-  },
-  tabBarOptions: {
-    activeTintColor: Platform.OS === 'ios' ? purple : white,
-    style: {
-      height: 56,
-      backgroundColor: Platform.OS === 'ios' ? white : purple,
-      shadowColor: 'rgba(0, 0, 0, 0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-})
+TMGStatusBar.propTypes = {
+	backgroundColor: PropTypes.string.isRequired,
+	barStyle: PropTypes.oneOf(['default', 'light-content', 'dark-content']),
+};
 
-const MainNavigator = StackNavigator({
-  Home: {
-    screen: Tabs,
-  },
-  EntryDetail: {
-    screen: EntryDetail,
-    navigationOptions: {
-      headerTintColor: white,
-      headerStyle: {
-        backgroundColor: purple,
-      }
-    }
-  }
-})
+type TabParamList = {
+	History: undefined;
+	AddEntry: undefined;
+	Live: undefined;
+};
+
+const TabNav: React.FC = () => {
+	/* eslint-disable react/display-name */
+	const RouteConfigs = {
+		History: {
+			name: 'History' as keyof TabParamList,
+			component: History,
+			options: {
+				tabBarIcon: ({ color }: { color: string }) => (
+					<Ionicons name="ios-bookmarks" size={30} color={color} />
+				),
+				title: 'History',
+			},
+		},
+		AddEntry: {
+			component: AddEntry,
+			name: 'Add Entry' as keyof TabParamList,
+			options: {
+				tabBarIcon: ({ color }: { color: string }) => (
+					<FontAwesome name="plus-square" size={30} color={color} />
+				),
+				title: 'Add Entry',
+			},
+		},
+		Live: {
+			component: Live,
+			name: 'Live' as keyof TabParamList,
+			options: {
+				tabBarIcon: ({ color }: { color: string }) => (
+					<Ionicons name="ios-speedometer" size={30} color={color} />
+				),
+				title: 'Live',
+			},
+		},
+	};
+	/* eslint-enable */
+
+	const TabNavigatorConfig = {
+		navigationOptions: {
+			header: null,
+		},
+		tabBarOptions: {
+			activeTintColor: Platform.OS === 'ios' ? purple : white,
+			style: {
+				height: 56,
+				backgroundColor: Platform.OS === 'ios' ? white : purple,
+				shadowColor: 'rgba(0, 0, 0, 0.24)',
+				shadowOffset: {
+					width: 0,
+					height: 3,
+				},
+				shadowRadius: 6,
+				shadowOpacity: 1,
+			},
+		},
+	};
+
+	// TypeScript bug prevents union of returnTypes for Tab
+	if (Platform.OS === 'ios') {
+		const Tab = createBottomTabNavigator<TabParamList>();
+		return (
+			<Tab.Navigator {...TabNavigatorConfig}>
+				<Tab.Screen {...RouteConfigs['History']} />
+				<Tab.Screen {...RouteConfigs['AddEntry']} />
+				<Tab.Screen {...RouteConfigs['Live']} />
+			</Tab.Navigator>
+		);
+	} else {
+		const Tab = createMaterialTopTabNavigator<TabParamList>();
+		return (
+			<Tab.Navigator {...TabNavigatorConfig}>
+				<Tab.Screen {...RouteConfigs['History']} />
+				<Tab.Screen {...RouteConfigs['AddEntry']} />
+				<Tab.Screen {...RouteConfigs['Live']} />
+			</Tab.Navigator>
+		);
+	}
+};
+
+type StackParamList = {
+	Home: undefined;
+	EntryDetail: undefined;
+};
+
+const MainNav: React.FC = () => {
+	const SCREEN = 'screen';
+
+	const StackNavigatorConfig = {
+		headerMode: SCREEN as typeof SCREEN,
+	};
+	const StackConfig = {
+		TabNav: {
+			name: 'Home' as keyof StackParamList,
+			component: TabNav,
+			options: { headerShown: false },
+		},
+		EntryDetail: {
+			name: 'EntryDetail' as keyof StackParamList,
+			component: EntryDetail,
+			options: {
+				headerTintColor: white,
+				headerStyle: {
+					backgroundColor: purple,
+				},
+				title: 'Entry Detail',
+			},
+		},
+	};
+	const Stack = createStackNavigator<StackParamList>();
+	return (
+		<Stack.Navigator {...StackNavigatorConfig}>
+			<Stack.Screen {...StackConfig['TabNav']} />
+			<Stack.Screen {...StackConfig['EntryDetail']} />
+		</Stack.Navigator>
+	);
+};
+
 const styles = StyleSheet.create({
 	mainAppView: {
 		flex: 1,
@@ -91,15 +174,18 @@ const styles = StyleSheet.create({
 const store: Store<RootState> = createStore(reducer);
 
 const App: React.FC = () => {
-	  componentDidMount() {
-    setLocalNotification()
-  }
+	React.useEffect(() => setLocalNotification(), []);
 
 	return (
 		<Provider store={store}>
 			<View style={styles.mainAppView}>
-				<TMGStatusBar backgroundColor={purple} barStyle="light-content" />
-				<MainNavigator />
+				<TMGStatusBar
+					backgroundColor={purple}
+					barStyle="light-content"
+				/>
+				<NavigationContainer>
+					<MainNav />
+				</NavigationContainer>
 			</View>
 		</Provider>
 	);
